706f881c11b23c5707ed62c0987994c9623b77d1
jinbolx <jinmin3401@gmail.com>
2017-10-31 17:32:59 +0800

    first secondbranch commit

--- app/src/main/java/com/example/jinmi/gitbranch/MainActivity.kt
+++ app/src/main/java/com/example/jinmi/gitbranch/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.jinmi.gitbranch
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"main",Toast.LENGTH_SHORT).show()
     }
 }
