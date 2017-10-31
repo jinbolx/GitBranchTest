2519e35f3b36877cd87aa07ec3678e3fb46628fc
jinbolx <jinmin3401@gmail.com>
2017-10-31 17:05:55 +0800

    add navi

--- app/src/main/java/com/example/jinmi/gitbranch/MainActivity.kt
+++ app/src/main/java/com/example/jinmi/gitbranch/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jinmi.gitbranch
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
@@ -8,5 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this,NaviActivity::class.java))
     }
 }
