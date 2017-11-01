3a39b8bb79552b12c1d6d714fac78b55664dee82
jinbolx <jinmin3401@gmail.com>
2017-11-01 09:25:14 +0800

    third branch

--- app/src/main/java/com/example/jinmi/gitbranch/MainActivity.kt
+++ app/src/main/java/com/example/jinmi/gitbranch/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jinmi.gitbranch
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,6 @@ class MainActivity : AppCompatActivity() {
        // setContentView(R.layout.activity_main)
         startActivity(Intent(this,NaviActivity::class.java))
         Toast.makeText(this,"main",Toast.LENGTH_SHORT).show()
+        Log.i("","")
     }
 }
